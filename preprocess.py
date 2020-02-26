541a77bf293fdf9ad5f4899ebaae49b049b86b60
aleriedy <aleriedy@users.noreply.github.com>
2020-02-26 10:43:07 +0000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -21,6 +21,7 @@ def new_normalize(input_file, output_file, shape, is_label):
     print(slice_in)
     data = np.pad(data, [(0, shape[i] - len_) for i, len_ in enumerate(data.shape)], "constant")
     if not is_label:
+        
         data = data[..., np.newaxis]
     print(data.shape)
     new_img = nib.Nifti1Image(data, img.affine)
@@ -37,7 +38,6 @@ def process(input_dir, output_dir, shape, is_label):
 
 if __name__ == '__main__':
     print("Begin preprocessing.")
-    # Modify the save path here, second parameter is temporary file path
     process('sample/image', 'temp/image', (192, 192, 160), False)
     process('sample/label', 'temp/label', (192, 192, 160), True)
     # The second parameter here is where preprocessed file are saved
